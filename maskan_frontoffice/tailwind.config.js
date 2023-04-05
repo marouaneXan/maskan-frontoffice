00df7e58c24a1b4141ea2de81a57a78ce8215bd5
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-05 22:48:39 +0000

    add theme for the app

--- maskan_frontoffice/tailwind.config.js
+++ maskan_frontoffice/tailwind.config.js
@@ -9,7 +9,30 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily:{
+      primary:"Poppins"
+    },
+    container:{
+      padding:{
+        DEFAULT:'1rem',
+        lg:'2rem'
+      }
+    },
+    screens:{
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1234px'
+    },
+    extend: {
+      colors:{
+        primary:'#101828',
+        secondary:'#7F56D9'
+      },
+      boxShadow:{
+        1:'0px 4px 30px rgba(0,0,0,0.8)'
+      }
+    },
   },
   plugins: [],
 }
