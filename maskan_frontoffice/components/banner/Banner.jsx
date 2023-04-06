e3d62555918c21f00a04e43544972449e9c70b1b
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-06 00:12:13 +0000

    create a navbar component

---
+++ maskan_frontoffice/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section>
+      <div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Banner
--- maskan_frontoffice/components/partials/navbar/Navbar.jsx
+++ maskan_frontoffice/components/partials/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../../public/maskan-logo.svg'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <header className='mb-12 border-b'>
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href='/'>
+                    <Image src={logo} alt="" className='w-[8rem] h-[6rem] object-fill' />
+                </Link>
+                <div className='flex items-center gap-6'>
+                    <Link className='hover:text-violet-900 transition' href='/login'>
+                        Login
+                    </Link>
+                    <Link className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition' href='/register'>
+                        Register
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Navbar
